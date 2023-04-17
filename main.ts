94667f6797c289018305cd32ef59c447075351e2
bocuypers <123274149+bocuypers@users.noreply.github.com>
2023-04-17 09:37:48 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.forever(function () {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 40)
+    basic.pause(5000)
+    maqueen.motorStop(maqueen.Motors.All)
+    basic.pause(2000)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 40)
+    basic.pause(5000)
+    maqueen.motorStop(maqueen.Motors.M1)
+    basic.pause(2000)
+})
